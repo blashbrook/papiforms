520e442303834db1671ae63a1e3fbe1ef2d3376c
Brian Lashbrook <blashbrook@gmail.com>
2020-12-21 17:05:43 -0600

    chore(prepare-database.sh): add shell script for development use

--- prepare-database.sh
+++ prepare-database.sh
@@ -8,27 +8,20 @@ pwd
 while true; do
     read -p "Is this the correct path?" yn
     case $yn in
-        [Yy]* ) 
+        [Yy]* )
         cp vendor/blashbrook/papiforms/src/Database/Seeders/DeliveryOptionSeeder.php  database/seeders/DeliveryOptionSeeder.php;
 		cp vendor/blashbrook/papiforms/src/Database/Seeders/MobilePhoneCarrierSeeder.php  database/seeders/MobilePhoneCarrierSeeder.php;
 		cp vendor/blashbrook/papiforms/src/Database/Seeders/PatronCodeSeeder.php  database/seeders/PatronCodeSeeder.php;
 		cp vendor/blashbrook/papiforms/src/Database/Seeders/PatronStatClassCodeSeeder.php  database/seeders/PatronStatClassCodeSeeder.php;
 		cp vendor/blashbrook/papiforms/src/Database/Seeders/PostalCodeSeeder.php  database/seeders/PostalCodeSeeder.php;
 		cp vendor/blashbrook/papiforms/src/Database/Seeders/UdfOptionSeeder.php  database/seeders/UdfOptionSeeder.php;
-		cp vendor/blashbrook/papiforms/src/Database/Seeders/UdfOptionDefSeeder.php  database/seeders/UdfOptionDefSeeder.php; 
+		cp vendor/blashbrook/papiforms/src/Database/Seeders/UdfOptionDefSeeder.php  database/seeders/UdfOptionDefSeeder.php;
 		break;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
-sed 's/namespace Blashbrook\\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/DeliveryOptionSeeder.php
-sed 's/namespace Blashbrook\\PAPIForms\\Database\\Seeders;/namespace Database\Seeders;/g' database/seeders/MobilePhoneCarrierSeeder.php
-sed 's/namespace Blashbrook\\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/PatronCodeSeeder.php
-sed 's/namespace Blashbrook\\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/PatronStatClassCodeSeeder.php
-sed 's/namespace Blashbrook\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/PostalCodeSeeder.php
-sed 's/namespace Blashbrook\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/UdfOptionDefSeeder.php
-sed 's/namespace Blashbrook\\PAPIForms\\Database\\Seeders;/namespace Database\\Seeders;/g' database/seeders/UdfOptionSeeder.php
-echo Creating database tables ...
+echo Seeding database tables ...
 php artisan db:seed --class=DeliveryOptionSeeder
 php artisan db:seed --class=MobilePhoneCarrierSeeder
 php artisan db:seed --class=PatronCodeSeeder
