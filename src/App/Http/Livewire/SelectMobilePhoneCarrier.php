c1007b5f5a0c57a0c14a3b83181f28ea25cf3ec3
Brian Lashbrook <blashbrook@gmail.com>
2020-12-22 13:35:09 -0600

    feat(livewire): add SelectMobilePhoneCarrier

---
+++ src/App/Http/Livewire/SelectMobilePhoneCarrier.php
@@ -0,0 +1,16 @@
+<?php
+
+use Livewire\Component;
+
+class SelectMobilePhoneCarrier extends Component
+{
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('livewire.select-mobile-phone-carrier');
+    }
+}
