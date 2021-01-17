289707f0fbb066df659d40b1f620384b2797e7a4
Brian Lashbrook <blashbrook@gmail.com>
2021-01-17 11:59:45 -0600

    fix(select-postal-code): add {{ $attributes }} to blade component

--- src/resources/views/components/input/select-postal-code.blade.php
+++ src/resources/views/components/input/select-postal-code.blade.php
@@ -3,7 +3,7 @@
     'postalCodes' => $this->postalCodes
 ])
 <div>
-    <select wire:model="selectedPostalCodeID" id="selectedPostalCodeID" name="selectedPostalCodeID" class = "form-input block w-full px-4 py-3 border
+    <select {{ $attributes }} class = "form-input block w-full px-4 py-3 border
             border-gray-300 rounded-md placeholder-gray-500 focus:outline-none
             focus:shadow-outline-blue focus:border-blue-300 transition duration-150
             ease-in-out">
