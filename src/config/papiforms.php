19dfa7725a72ae1ba7bee3764103d8624c700347
Brian Lashbrook <blashbrook@gmail.com>
2021-03-15 09:02:38 -0500

    feat(papiforms config):add patron_uploads folder to config file

--- src/config/papiforms.php
+++ src/config/papiforms.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    //
+    'patron_uploads' => [
+        'driver' => 'local',
+        'root' => storage_path('app/patron_uploads'),
+        'url' => env('APP_URL') . '/patron_uploads',
+        'visibility' => 'public',
+    ]
 ];
