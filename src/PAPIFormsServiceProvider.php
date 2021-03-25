b9709c7ef72100291cd119ca089cac22449b9733
Brian Lashbrook <blashbrook@gmail.com>
2021-03-25 10:25:32 -0500

    feat(s3): modify .env and move uploads to s3

--- src/PAPIFormsServiceProvider.php
+++ src/PAPIFormsServiceProvider.php
@@ -84,14 +84,17 @@ class PAPIFormsServiceProvider extends ServiceProvider
         // Dynamically configure uploads disks and links
         Config::set('filesystems.disks.uploads',
         [
-            'driver' => 'local',
-            'root' => storage_path('app/uploads'),
-            'url' => env('APP_URL').'/uploads',
-            'visibility' => 'public',
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET_UPLOADS'),
+            'url' => 'https://apigateway.us-east-2.amazonaws.com',
+            'endpoint' => 's3.us-east-2.amazonaws.com'
         ]);
-        Config::set('filesystems.links',
+/*        Config::set('filesystems.links',
             [public_path('uploads') => storage_path('app/uploads')]
-        );
+        );*/
     }
 
     /**
