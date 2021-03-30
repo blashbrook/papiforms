35404cc97e3d02d5c6321c2ef518d389dec5888c
Brian Lashbrook <blashbrook@gmail.com>
2021-03-30 01:08:02 -0500

    fix(uploads): add metadata to app.blade.php to upgrade blocked livewire uploads from http to https

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
