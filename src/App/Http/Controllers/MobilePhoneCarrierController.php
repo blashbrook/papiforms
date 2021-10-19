e76148c1c72916c839dce83abb6881644b166832
blashbrook <blashbrook@users.noreply.github.com>
2021-10-19 17:02:59 +0000

    Apply fixes from StyleCI

--- src/App/Http/Controllers/MobilePhoneCarrierController.php
+++ src/App/Http/Controllers/MobilePhoneCarrierController.php
@@ -38,8 +38,7 @@ class MobilePhoneCarrierController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     *
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -50,8 +49,7 @@ class MobilePhoneCarrierController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param \App\Models\MobilePhoneCarrier $mobilePhoneCarrier
-     *
+     * @param  \App\Models\MobilePhoneCarrier  $mobilePhoneCarrier
      * @return \Illuminate\Http\Response
      */
     public function show(MobilePhoneCarrier $mobilePhoneCarrier)
@@ -62,8 +60,7 @@ class MobilePhoneCarrierController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param \App\Models\MobilePhoneCarrier $mobilePhoneCarrier
-     *
+     * @param  \App\Models\MobilePhoneCarrier  $mobilePhoneCarrier
      * @return \Illuminate\Http\Response
      */
     public function edit(MobilePhoneCarrier $mobilePhoneCarrier)
@@ -74,9 +71,8 @@ class MobilePhoneCarrierController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request       $request
-     * @param \App\Models\MobilePhoneCarrier $mobilePhoneCarrier
-     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MobilePhoneCarrier  $mobilePhoneCarrier
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, MobilePhoneCarrier $mobilePhoneCarrier)
@@ -87,8 +83,7 @@ class MobilePhoneCarrierController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param \App\Models\MobilePhoneCarrier $mobilePhoneCarrier
-     *
+     * @param  \App\Models\MobilePhoneCarrier  $mobilePhoneCarrier
      * @return \Illuminate\Http\Response
      */
     public function destroy(MobilePhoneCarrier $mobilePhoneCarrier)
