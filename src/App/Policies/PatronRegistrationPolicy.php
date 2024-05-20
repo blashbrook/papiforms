7deca1b7eae40c8b11e80ba1713f9d4d0e738c14
blashbrook <blashbrook@users.noreply.github.com>
2024-05-19 23:22:41 -0500

    fix: code style patch

--- src/App/Policies/PatronRegistrationPolicy.php
+++ src/App/Policies/PatronRegistrationPolicy.php
@@ -2,86 +2,86 @@
 
 namespace Blashbrook\PAPIForms\App\Policies;
 
-    use App\User;
-    use Blashbrook\PAPIForms\App\Models\PatronRegistration;
-    use Illuminate\Auth\Access\HandlesAuthorization;
+use App\User;
+use Blashbrook\PAPIForms\App\Models\PatronRegistration;
+use Illuminate\Auth\Access\HandlesAuthorization;
 
-    class PatronRegistrationPolicy
+class PatronRegistrationPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any patron registrations.
+     *
+     * @param  User  $user
+     * @return bool
+     */
+    public function viewAny(User $user): bool
     {
-        use HandlesAuthorization;
-
-        /**
-         * Determine whether the user can view any patron registrations.
-         *
-         * @param  User  $user
-         * @return bool
-         */
-        public function viewAny(User $user): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can view the patron registration.
-         *
-         * @param  User  $user
-         * @param  PatronRegistration  $patronRegistration
-         * @return bool
-         */
-        public function view(User $user, PatronRegistration $patronRegistration): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can create patron registrations.
-         *
-         * @param  User  $user
-         * @return bool
-         */
-        public function create(User $user): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can update the patron registration.
-         *
-         * @param  User  $user
-         * @param  PatronRegistration  $patronRegistration
-         * @return bool
-         */
-        public function update(User $user, PatronRegistration $patronRegistration): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can delete the patron registration.
-         *
-         * @param  User  $user
-         * @param  PatronRegistration  $patronRegistration
-         * @return bool
-         */
-        public function delete(User $user, PatronRegistration $patronRegistration): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can restore the patron registration.
-         *
-         * @param  User  $user
-         * @param  PatronRegistration  $patronRegistration
-         * @return bool
-         */
-        public function restore(User $user, PatronRegistration $patronRegistration): bool
-        {
-        }
-
-        /**
-         * Determine whether the user can permanently delete the patron registration.
-         *
-         * @param  User  $user
-         * @param  PatronRegistration  $patronRegistration
-         * @return bool
-         */
-        public function forceDelete(User $user, PatronRegistration $patronRegistration): bool
-        {
-        }
     }
+
+    /**
+     * Determine whether the user can view the patron registration.
+     *
+     * @param  User  $user
+     * @param  PatronRegistration  $patronRegistration
+     * @return bool
+     */
+    public function view(User $user, PatronRegistration $patronRegistration): bool
+    {
+    }
+
+    /**
+     * Determine whether the user can create patron registrations.
+     *
+     * @param  User  $user
+     * @return bool
+     */
+    public function create(User $user): bool
+    {
+    }
+
+    /**
+     * Determine whether the user can update the patron registration.
+     *
+     * @param  User  $user
+     * @param  PatronRegistration  $patronRegistration
+     * @return bool
+     */
+    public function update(User $user, PatronRegistration $patronRegistration): bool
+    {
+    }
+
+    /**
+     * Determine whether the user can delete the patron registration.
+     *
+     * @param  User  $user
+     * @param  PatronRegistration  $patronRegistration
+     * @return bool
+     */
+    public function delete(User $user, PatronRegistration $patronRegistration): bool
+    {
+    }
+
+    /**
+     * Determine whether the user can restore the patron registration.
+     *
+     * @param  User  $user
+     * @param  PatronRegistration  $patronRegistration
+     * @return bool
+     */
+    public function restore(User $user, PatronRegistration $patronRegistration): bool
+    {
+    }
+
+    /**
+     * Determine whether the user can permanently delete the patron registration.
+     *
+     * @param  User  $user
+     * @param  PatronRegistration  $patronRegistration
+     * @return bool
+     */
+    public function forceDelete(User $user, PatronRegistration $patronRegistration): bool
+    {
+    }
+}
